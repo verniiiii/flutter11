aad7d39d42957f5805d3a559c6c20c16a79a6582
Shestopalova Veronika <vera.shestopalova@gmail.com>
2025-12-09 23:02:43 +0300

    Domain Layer - use cases (остальные модули)

---
+++ lib/domain/usecases/cards/add_card_usecase.dart
@@ -0,0 +1,12 @@
+import '../../repositories/card_repository.dart';
+import '../../entities/card_entity.dart';
+
+class AddCardUseCase {
+  final CardRepository repository;
+
+  AddCardUseCase(this.repository);
+
+  Future<void> call(CardEntity card) {
+    return repository.addCard(card);
+  }
+}
---
+++ lib/domain/usecases/cards/get_cards_usecase.dart
@@ -0,0 +1,12 @@
+import '../../repositories/card_repository.dart';
+import '../../entities/card_entity.dart';
+
+class GetCardsUseCase {
+  final CardRepository repository;
+
+  GetCardsUseCase(this.repository);
+
+  Future<List<CardEntity>> call() {
+    return repository.getCards();
+  }
+}
