58cab8c9b774324b332e0f51b9e5245effa95f26
Shestopalova Veronika <vera.shestopalova@gmail.com>
2025-12-09 22:59:46 +0300

    Domain Layer - entities

---
+++ lib/domain/entities/motivation_entity.dart
@@ -0,0 +1,19 @@
+import 'package:flutter11/core/models/motivation_model.dart';
+
+class MotivationContentEntity {
+  final String id;
+  final String text;
+  final String? author;
+  final ContentType type;
+  final String category;
+  final bool isFavorite;
+
+  MotivationContentEntity({
+    required this.id,
+    required this.text,
+    this.author,
+    required this.type,
+    required this.category,
+    this.isFavorite = false,
+  });
+}
