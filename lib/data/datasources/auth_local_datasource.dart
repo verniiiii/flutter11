f6ac414305346943daa8e0e770eb91fa1e8ccc57
Shestopalova Veronika <vera.shestopalova@gmail.com>
2025-12-09 23:03:28 +0300

    Data Layer - datasources

---
+++ lib/data/datasources/auth_local_datasource.dart
@@ -0,0 +1,17 @@
+import 'package:flutter11/core/models/user_model.dart';
+
+class AuthLocalDataSource {
+  User? _currentUser;
+
+  Future<User?> getCurrentUser() async {
+    return _currentUser;
+  }
+
+  Future<void> setCurrentUser(User? user) async {
+    _currentUser = user;
+  }
+
+  Future<void> clearUser() async {
+    _currentUser = null;
+  }
+}
